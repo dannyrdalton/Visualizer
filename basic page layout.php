08bf592ceac074688d55611610e792b0a00166fb
Danny Dalton <drdalton@stanford.edu>
2012-10-25 19:49:53 -0700

    Initial Commit

---
+++ basic page layout.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html> 
+<html>
+
+<head>
+	<title>VoVizenVision</title> 
+	<meta charset="utf-8">
+	<meta name="apple-mobile-web-app-capable" content="yes">
+ 	<meta name="apple-mobile-web-app-status-bar-style" content="black">
+	<meta name="viewport" content="width=device-width, initial-scale=1"> 
+
+	<link rel="stylesheet" href="jquery.mobile-1.2.0.css" /> //link to style sheet
+
+	<link rel="stylesheet" href="style.css" /> //links to CSS style sheet
+	<link rel="apple-touch-icon" href="appicon.png" /> //file for app icon
+	<link rel="apple-touch-startup-image" href="upstart.png"> //file for startup image
+	
+	<script src="jquery-1.8.2.min.js"></script>
+	<script src="jquery.mobile-1.2.0.js"></script>
+	
+	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js"></script> //ajax
+
+</head>
+
+<body>
+
+
+<!-- Page Begins Here -->
+<div data-role="page" id="home">
+
+	<!-- Header Begins Here -->
+	<div data-role="header">
+		<h1>Header Text</h1>
+	</div>
+	<!-- Header Ends Here->
+	
+	
+	<!-- Content Begins Here -->
+	<div data-role="content">
+	
+	</div>
+	<!-- Content Ends Here -->
+	
+	
+	<!-- Footer Begins Here -->
+	<div data-role="footer">
+	
+	</div>
+	<!-- Footer Ends Here -->
+</div>
+<!-- Page Ends Here -->
+
+</body>
+</html>
+
