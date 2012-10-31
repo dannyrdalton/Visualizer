0457b02327e85a62da7a9cba44a38b62efe07763
Scott Takahashi <hapajazz@gmail.com>
2012-10-31 00:06:16 -0700

    Changed to Express 2

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var express = require('express'),
-    app = express(),
+    app = express.createServer(express.logger()),
     io = require('socket.io').listen(app),
     routes = require('./routes');
 
