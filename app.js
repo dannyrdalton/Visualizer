d2419f7fcf790ba95efd48a730f3d3ac2a11ea42
Scott Takahashi <hapajazz@gmail.com>
2012-11-07 20:30:05 -0800

    going back to heroku

--- app.js
+++ app.js
@@ -36,19 +36,17 @@ pg.connect(process.env.DATABASE_URL, function(err, client) {
 // Heroku won't actually allow us to use WebSockets
 // so we have to setup polling instead.
 // https://devcenter.heroku.com/articles/using-socket-io-with-node-js-on-heroku
-/*
 
 io.configure(function () {
   io.set("transports", ["xhr-polling"]);
   io.set("polling duration", 10);
   io.set("close timeout", 11);
 }); 
-*/
 
 
 // Routes
 
-var port = 5000;//process.env.PORT || 5000; // Use the port that Heroku provides or default to 5000
+var port = process.env.PORT || 5000; // Use the port that Heroku provides or default to 5000
 app.listen(port, function() {
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 });
